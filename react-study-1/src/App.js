7e4c0928185b139034e594f9263a976bedb1c919
unknown <kwakms821@daum.net>
2020-05-05 01:25:28 +0900

    add Components header,content,footer

--- react-study-1/src/App.js
+++ react-study-1/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Header from './component/Header';
+import Footer from './component/Footer';
+import Content from './component/Content';
 
 function App() {
   return (
     <>
       <Header />
+      <Content />
+      <Footer />
     </>
   );
 }
---
+++ react-study-1/src/component/Content.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Content(){
+    return <div id="content">내용</div>;
+}
+
+export default Content;
